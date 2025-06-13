7ce677c3450e92702adac89c7baeb6bf3e4bfd5c
likespro <likespro.eth@gmail.com>
2025-06-13 11:03:51 +0300

    Add Repository.kt interface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ### New features
 `core` module:
 - Add `Pagination` class to work with pagination
+- Add `Repository` interface with common repository methods
 `core-mit` module
 - Add `DetailsConfiguration` for `WrappedException.kt` and `EncodableResult.kt` to customize exception details inclusion
 - Added `randomBase64()` method to generate random Base64 of the specified length
---
+++ core/src/main/java/eth/likespro/commons/models/Repository.kt
@@ -0,0 +1,23 @@
+package eth.likespro.commons.models
+
+import eth.likespro.commons.Pagination
+
+interface Repository<Entity : eth.likespro.commons.models.Entity<ID>, ID : Any> {
+    suspend fun findById(id: ID): Entity?
+
+    suspend fun isExisting(id: ID): Boolean
+
+    suspend fun findAll(): List<Entity>
+
+    suspend fun findAll(pagination: Pagination): Pagination.Result<Entity>
+
+    suspend fun create(entity: Entity): Entity
+
+    suspend fun upsert(entity: Entity): Entity
+
+    suspend fun update(entity: Entity): Entity?
+
+    suspend fun delete(id: ID)
+
+    suspend fun count(pagination: Pagination? = null): Long
+}
